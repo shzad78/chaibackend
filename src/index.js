1d05a9ca21cc9883ef2599d0e8d5211ab710db4c
shzad78 <shzad78@gmail.com>
2024-12-17 12:51:57 +0100

    jfda

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+( async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+    } catch (error) {
+        console.log('ERROR: ', error)
+    }
+    
+}
+
+)()
